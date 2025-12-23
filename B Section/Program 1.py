aeac032d9f63c88f06a6b039d79bc2a7737dd7d8
Rohith Vishwa <rohithvishwa.p2005@gmail.com>
2025-12-24 00:00:41 +0530

    File Upload

--- B Section/Program 1.py
+++ B Section/Program 1.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def fact(n):
     if n==0 or n==1:
         return 1
@@ -15,4 +16,26 @@ def repeat():
 
 n = int(input("Enter the number : "))
 print(f"The Factorial of {n} is : ",fact(n))
+=======
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def repeat():
+    a = input("\nDo you want to continue yes or no : ")
+    if a == "yes":
+        n = int(input("Enter a number: "))
+        result = factorial(n)
+        print("Factorial of", n, "is", result)
+        repeat()
+    else:
+        print("Program Stopped")
+    
+    
+n = int(input("Enter a number: "))
+result = factorial(n)
+print("Factorial of", n, "is", result)
+>>>>>>> Stashed changes
 repeat()
--- B Section/Program 2.py
+++ B Section/Program 2.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 def fibo(n):
     if n==0 or n==1:
         return 1
@@ -15,4 +16,26 @@ def repeat():
 
 n = int(input("Enter the number : "))
 print(f"The Fibonacci number of {n} is : ",fibo(n))
+=======
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def repeat():
+    a = input("\nDo you want to continue yes or no : ")
+    if a == "yes":
+        n = int(input("Enter a number: "))
+        result = fibonacci(n)
+        print("Fibonacci of", n, "is", result)
+        repeat()
+    else:
+        print("Program Stopped")
+    
+    
+n = int(input("Enter a number: "))
+result = fibonacci(n)
+print("Fibonacci of", n, "is", result)
+>>>>>>> Stashed changes
 repeat()
